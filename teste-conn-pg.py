301e4d7ecbb223cb5aa6e0d1074608c998a5924b
Mario Azevedo <marioazvdo55@gmail.com>
2019-04-18 13:16:34 +0000

    teste-conn-pg.py

--- teste-conn-pg.py
+++ teste-conn-pg.py
@@ -11,7 +11,7 @@ import subprocess
 def  connect_db( ):	
     try:
         conn = psycopg2.connect("dbname='_@DBNAME@_' user='_@USR@_' host='_@HOST@_'  port='_@PORT@_' password='_@PWD@_'")
-#        conn = psycopg2.connect("dbname='valemobi' user='Valemobi' host='192.168.80.201'  port='15432' password=''")
+#        conn = psycopg2.connect("dbname='' user='' host=''  port='' password=''")
         conn.close()
         return True
     except:
